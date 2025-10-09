aa481aa1f9e154388aa6cf7f0969ea601831c89b
mwangi26145-dev <mwangi.26145@students.kyu.ac.ke>
2025-10-09 23:13:27 +0300

    Add files via upload

---
+++ No._50-100.c
@@ -0,0 +1,17 @@
+/*
+Name:Mwangi Samuel
+Reg No.:CT100/G/26145/25
+Description:Prints No. 50-100 in descending order 
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i;
+    
+    for(i = 100; i >= 50; i--) {
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
